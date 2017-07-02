2b60ef5b4901e298882c0fcc1eb2d90429589465
RedHunter <arunasanjeewa1994@gmail.com>
2017-07-03 01:31:21 +0530

    first

---
+++ src/app/components/Home.js
@@ -0,0 +1,18 @@
+/**
+ * Created by RedHunter on 6/18/2017.
+ */
+import React from "react";
+
+export class Home extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h3>
+                    Home
+                </h3>
+            </div>
+
+        );
+    }
+}
---
+++ src/app/components/Root.js
@@ -0,0 +1,27 @@
+/**
+ * Created by RedHunter on 6/19/2017.
+ */
+import React from "react";
+
+import {Header} from "./Header"
+export class Root extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header/>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
